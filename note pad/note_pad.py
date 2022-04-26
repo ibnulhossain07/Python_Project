58bedd822dbaf98f0b7ce83822a7ed17a5394eaa
Ibnul Hossain <ibnulhossain07@gmail.com>
2022-04-26 12:18:23 +0600

    first commit

---
+++ note pad/note_pad.py
@@ -0,0 +1,11 @@
+from tkinter.filedialog import *
+import tkinter as tk
+
+
+canvas = tk.Tk()
+canvas.geometry("400x400")
+canvas.title("Notepad Editor")
+canvas.config(bg="black")
+
+
+canvas.mainloop()
